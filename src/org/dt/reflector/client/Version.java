062d589a1e2d11b2c37381286811a7cbf8f3b99e
David Sykes <dave.expat@gmail.com>
2014-06-24 15:48:01 +1000

    merge-issue-11

--- src/org/dt/reflector/client/Version.java
+++ src/org/dt/reflector/client/Version.java
@@ -1,5 +1,5 @@
 package org.dt.reflector.client;
 
 public class Version {
-  public static final String getVersion() { return "0.3.2"; }
+  public static final String getVersion() { return "0.3.3"; }
 }
